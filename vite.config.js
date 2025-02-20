075a43db460236f469fca569b5160b8a52900e35
umanghere <umangbansal1st@gmail.com>
2025-02-20 21:34:53 +0530

    Changed vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/Team-Service-UI/",
   resolve: {
     alias: {
       'date-fns/_lib/format/longFormatters': path.resolve(__dirname, 'node_modules/date-fns/_lib/format/longFormatters'),
